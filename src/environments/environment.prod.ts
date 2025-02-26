77a37be2220724fa019d389cde6927ef959616dc
alinojcreators <alinojcreators@gmail.com>
2025-02-26 22:18:20 +0530

    maximum size exceed

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,6 @@
 export const environment = {
   production: true,
   apiUrl: 'http://54.166.102.55/api',
-  uploadsUrl: 'http://54.166.102.55/uploads'
+  baseUrl: 'http://54.166.102.55/uploads'
+  
 };
